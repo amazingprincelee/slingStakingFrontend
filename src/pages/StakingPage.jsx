9163c1f2a7841eba191fee919a070d1c42071707
Prince Lee Nna <prince_lee.nna85@ymail.com>
2023-04-28 15:42:49 +0100

    Update StakingPage.jsx

--- src/pages/StakingPage.jsx
+++ src/pages/StakingPage.jsx
@@ -4,7 +4,6 @@ import { Carousel } from "react-bootstrap";
 import newsImage1 from "../images/news.jpeg";
 import newsImage2 from "../images/sling-logo.png";
 
-
 function StakingPage() {
   const carouselItems = [
     {
@@ -39,73 +38,48 @@ function StakingPage() {
 
   return (
     <div>
-      <div class="container">
+      <div class="container mt-5">
         <div class="container text-center">
-          <div class="row align-items-start">
-            <div class="col">
-              <div className="token-balance box-showdown m-2">
-                <p>Staking Balance</p>
-                <p>0 SLING</p>
-              </div>
+          <div class="row align-items-center">
+            <div class="col-3-lg col-lg-6 border rounded bg-warning border-warning m-2 mx-auto">
+              <p>Staking Balance</p>
+              <p>0 SLING</p>
             </div>
-            <div class="col">
-              <div className="token-balance box-showdown m-2">
-                <p>Reward Balance</p>
-                <p>0 SLING</p>
-              </div>
+            <div class="col-3-lg col-lg-6 border rounded bg-warning border-warning m-2 mx-auto">
+              <p>Reward Balance</p>
+              <p>0 SLING</p>
             </div>
-            <div class="col">
-              <div className="news-update box-showdown carousel m-2">
-                <Carousel>
-                  {carouselItems.map((item) => (
-                    <Carousel.Item key={item.id}>
-                      <div className="row">
-                        <div className="col-md-4">
-                          <img
-                            className="d-block w-100"
-                            src={item.image}
-                            alt={item.title}
-                          />
-                        </div>
-                        <div className="col-md-8 mb-3">
-                          <h3>{item.title}</h3>
-                          <p>{item.description}</p>
-                        </div>
-                      </div>
-                    </Carousel.Item>
-                  ))}
-                </Carousel>
-              </div>
+            <div className="col-3-lg border rounded bg-warning border-warning m-2 mx-auto">
+              <p>Token Balanced</p>
+              <p>0 SLING</p>
             </div>
           </div>
         </div>
       </div>
       <div class="container text-center">
-  <div class="row align-items-center">
-    <div class="col">
-    <div class="container mb-3 ">
-        <div className="staking-form box-showdown">
-          <h1>STAKE SLING</h1>
-          <p>Duration: 90 Days</p>
-          <input type="number" placeholder="Amount to stake" />
-          <div className="percentage-buttons">
-            <button className="percentage-button">25%</button>
-            <button className="percentage-button">50%</button>
-            <button className="percentage-button">75%</button>
-            <button className="percentage-button">Max</button>
+        <div class="row align-items-center">
+          <div class="col">
+            <div class="container mb-5 ">
+              <div className="staking-form box-showdown">
+                <h1>STAKE SLING</h1>
+                <p>Duration: 90 Days</p>
+                <input type="number" placeholder="Amount to stake" />
+                <div className="percentage-buttons">
+                  <button className="percentage-button">25%</button>
+                  <button className="percentage-button">50%</button>
+                  <button className="percentage-button">75%</button>
+                  <button className="percentage-button">Max</button>
+                </div>
+                <button className="submit-button">DEPOSIT</button>
+                <button className="submit-button">WiTHDRAW</button>
+              </div>
+            </div>
           </div>
-          <button className="submit-button">DEPOSIT</button>
-          <button className="submit-button">WiTHDRAW</button>
+          <div class="col mb-5">
+            
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col mb-5">
-    Sling is a decentralized finance (DeFi) platform that offers a staking <br/> app with an impressive 25% annual percentage rate
-    </div>
-    
-  </div>
-</div>
-      
     </div>
   );
 }
